2481b36570d6a24d9db21f0069696596ac40d069
Ayman Berkane <302126671@student.rocmondriaan.nl>
2024-09-06 15:21:24 +0200

    Add basic structure for Hotel Project homepage
    HTML Structure: Created the initial HTML structure for the Hotel Project homepage, including a header section with a responsive navigation bar.
    Bootstrap Integration: Integrated Bootstrap 5.3.3 for responsive design, including linking to the Bootstrap CSS and JS files via CDN.
    Header Section:
    Added a header with a dark background.
    Included a project title "Hotel Project" aligned to the left.
    Added a navigation bar aligned to the right with placeholder links for future sections.
    Used Bootstrap grid system (row and col) for layout and positioning.
    Styling: Linked an external stylesheet (css/style.css) for future custom styles

---
+++ project-hotel/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 p-5 bg-dark">
+                    <div class="row">
+                        <div class="col-2">
+                            <h2 class="text-white d-flex justify-content-center flex-column">Hotel project</h2>
+                        </div>
+                        <div class="col-4">
+
+                        </div>
+                        <div class="col-6">
+                            <ul class="nav d-flex justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

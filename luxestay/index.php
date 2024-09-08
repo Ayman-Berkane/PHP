f6c1e095c9ca947569cf4aabd0af3b12b206f890
Ayman Berkane <302126671@student.rocmondriaan.nl>
2024-09-08 12:17:24 +0200

    Added Homepage Structure and Carousel for LuxeStay
    Implemented the main structure for the LuxeStay homepage with a fixed-top navigation bar.
    
    Added a responsive carousel component showcasing images of rooms and amenities.
    
    Integrated Bootstrap 5.3.3 for styling and responsiveness.
    
    Included a call-to-action overlay on the carousel with a "Get Started" button.

---
+++ luxestay/index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LuxeStay</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <style>
+
+    </style>
+</head>
+<body>
+<div class="header">
+    <div class="container-fluid">
+        <div class="row bg-dark bg-opacity-50 p-4 fixed-top">
+            <div class="col-2 d-flex justify-content-start align-items-center">
+                <h2 class="text-white">LuxeStay</h2>
+            </div>
+            <div class="col-9">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler ms-auto bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav d-flex justify-content-around align-items-center w-100">
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" aria-current="page" href="#">HOME</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">ROOMS</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">LOCATION</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">ACTIVITIES</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">RESTAURANT</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">GALLERY</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">BLOG</a>
+                                </li>
+                                <li class="nav-item fs-6 mt-2">
+                                    <a class="nav-link text-opacity-100 text-white" href="#">CONTACT</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="main">
+    <div class="container-fluid bg-dark">
+        <div class="row no-gutters">
+            <div class="col-xs-12 p-0 position-relative">
+                <div id="carouselExampleDark" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="10000">
+                            <img src="https://www.welcome-hotels.com/site/assets/files/84580/welcome_hotel_darmstadt_musterziimmer_1.2560x1600.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                            <img src="https://www.primalstrength.com/cdn/shop/files/gymdesign_render_Two_collumn_grid_cb1b5850-fa8e-4a7b-a2b3-190c2e45facd.jpg?v=1680719688&width=1500" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://static11.com-hotel.com/uploads/hotel/304387/photo/hotel-sb-bcn-events-4-sup_15805635281.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <div class="position-absolute d-flex justify-content-center align-items-center">
+                    <div class="cont">
+                        <h5 class="text-opacity-100">Discover Your Perfect Escape</h5>
+                        <h2 class="p-2 text-opacity-100">Experience Unmatched Comfort and Elegance<br>in the Heart of Luxury</h2>
+                        <div class="card-body d-flex justify-content-center p-3">
+                            <button type="button" class="btn btn-dark text-white p-3 rounded-pill">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
